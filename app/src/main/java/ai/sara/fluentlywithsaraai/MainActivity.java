a45078ae74d1bffa91bb0457045bf184df674e86
rfugal <rfugal@gmail.com>
2017-02-04 22:01:52 -0700

    Create Reader Interface #14 v0.02 on Play Store Partial implementation of #11

--- app/src/main/java/ai/sara/fluentlywithsaraai/MainActivity.java
+++ app/src/main/java/ai/sara/fluentlywithsaraai/MainActivity.java
@@ -1,9 +1,12 @@
 package ai.sara.fluentlywithsaraai;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,25 +19,58 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.regex.Pattern;
 
 import ai.sara.fluentlywithsaraai.data.User;
 import ai.sara.fluentlywithsaraai.data.UserListContract;
 import ai.sara.fluentlywithsaraai.data.UsersDb;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "Kx5Jdpi3lU8LpNmSPi5NDrims";
+    private static final String TWITTER_SECRET = "DXdaUkSiCs6oNHkN0TlGuBiFK5pZP2iBOreFNWHBN1M9qP2OHg";
+
     private ArrayList<User> Users = new ArrayList<>(0);
+    private final String USER_SESSION = "CurrentUser";
+    private final String USER_ID = "UserId";
+    private final String USER_NAME = "UserName";
+    private SharedPreferences sharedpreferences;
+    private int saraIndex = 0;
+    private String[] saraSays;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        sharedpreferences = getSharedPreferences(USER_SESSION, Context.MODE_PRIVATE);
         setContentView(R.layout.activity_main);
+        saraSays = getResources().getStringArray(R.array.sara_main);
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         UsersDb db = new UsersDb(this);
         Cursor c = db.rawQuery("SELECT * FROM " + UserListContract.UserList.TABLE_NAME,
                 new String[] {});
+        if (c.getCount() < 1) {
+            User example = new User(this,getString(R.string.example_user_name),new GregorianCalendar(2012,12,12),UserListContract.UserList.GENDER_MALE,getString(R.string.example_known_words));
+            listUser(example,getString(R.string.example_user_name));
+        }
         if (c.moveToFirst()) {
             do {
                 User user = new User(this, c.getString(c.getColumnIndex(UserListContract.UserList.COLUMN_USER_ID)));
@@ -84,7 +120,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         String name = newUserName.getText().toString();
                         int year;
-                        int gender = 0;
+                        int month = newUserBirthmonth.getSelectedItemPosition();
+                        int day = newUserBirthday.getSelectedItemPosition();
+                        int gender = newUserGender.getSelectedItemPosition();
                         try {
                             year = Integer.getInteger(newUserBirthyear.getSelectedItem().toString());
                         } catch(Exception e) {
@@ -95,7 +133,13 @@ public class MainActivity extends AppCompatActivity {
                                 year = 2003;
                             }
                         }
-                        User user = new User(MainActivity.this,name,year,gender);
+                        if (month == 0) {
+                            month = 12;
+                            day = 31;
+                        }
+                        if (day == 0) day = 1;
+                        GregorianCalendar birthdate = new GregorianCalendar(year,month,day);
+                        User user = new User(MainActivity.this,name,birthdate,gender);
                         listUser(user,name);
                     }
                 });
@@ -106,6 +150,19 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 addUserAlert.show();
+            }
+        });
+        Toast pressSara = Toast.makeText(this, getString(R.string.sara_main_toast), Toast.LENGTH_LONG);
+        pressSara.show();
+        FloatingActionButton sara = (FloatingActionButton) findViewById(R.id.sara_main);
+        sara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!tts.isSpeaking()) {
+                    tts.speak(saraSays[saraIndex],TextToSpeech.QUEUE_FLUSH,null);
+                    saraIndex++;
+                    if (saraIndex == saraSays.length) saraIndex = 0;
+                }
             }
         });
     }
@@ -133,9 +190,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openUser(View view, User user) {
-        Intent i = new Intent(this, userZachary.class);
-        i.putExtra("USER_ID", user.getId());
-        i.putExtra("USER_NAME", user.getUserName());
+        Intent i = new Intent(this, UserHomeActivity.class);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(USER_ID,user.getId());
+        editor.putString(USER_NAME,user.getUserName());
+        editor.commit();
         startActivity(i);
     }
     public void listUser(User user, final String username){
@@ -150,7 +209,38 @@ public class MainActivity extends AppCompatActivity {
                 openUser(view, Users.get(view.getId()));
             }
         });
+        newUser.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(final View view) {
+                final AlertDialog.Builder deleteUserAlert = new AlertDialog.Builder(MainActivity.this);
+                deleteUserAlert.setTitle("Delete User");
+                final User dUser = Users.get(view.getId());
+                TextView dConfirm = new TextView(MainActivity.this);
+                dConfirm.setText("Are you sure you want to delete\n\t" + dUser.getUserName() + "\n\nTHIS CANNOT BE UNDONE!");
+                dConfirm.setPadding(24,24,24,24);
+                deleteUserAlert.setView(dConfirm);
+                deleteUserAlert.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        view.setVisibility(View.GONE);
+                        dUser.deleteUser();
+                    }
+                });
+                deleteUserAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                deleteUserAlert.show();
+                return true;
+            }
+        });
         LinearLayout userList = (LinearLayout) findViewById(R.id.user_list);
         userList.addView(newUser);
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        tts.shutdown();
     }
 }
